764a58c8212a878c7ee346357f70f873a29e1cf8
Kishan Patel <mail@kishanpatel.dev>
2025-04-16 09:29:21 -0500

    Update route tests

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -6,15 +6,11 @@ def test_home_route(client):
 
 def test_weather_route(client):
     response = client.get("/weather")
-    print(response.data)
-    print(client)
-    for rule in client.application.url_map.iter_rules():
-        print(f"{rule}: {rule.endpoint}")
     assert response.status_code == 200
-    assert b"Loading weather..." not in response.data
+    assert b"Last updated" in response.data
 
 
 def test_events_route(client):
     response = client.get("/events")
     assert response.status_code == 200
-    assert b"Loading events..." not in response.data
+    assert b"Last updated" in response.data
